2ebf2dd1273e83c6cd6dc8f616f27ebbc6873e98
Scott Nath <github@scottnath.com>
2015-05-29 13:43:25 -0400

    compilers moved to utilities repo

--- index.js
+++ index.js
@@ -8,7 +8,5 @@ module.exports = {
   patternImporter: require('./lib/pattern-importer'),
   utils: require('./lib/utils'),
   importSinglePattern: require('./lib/import-single-pattern'),
-  twigCompiler: require('./lib/pattern-compilers/twig-compiler'),
-  sassCompiler: require('./lib/css-compilers/sass-compiler'),
   gulpImportPatterns: require('./gulp/patterns-import')
 }
